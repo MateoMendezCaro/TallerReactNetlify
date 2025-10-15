934154a6807de980e02cc1f26a74698680451552
tuNombre <tucorreo@example.com>
2025-10-15 07:51:23 -0500

    agregado de receta 1

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import NavBar from './components/NavBar'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Arepas from './pages/receta1'
-import Brownies from './pages/receta2'
+import PolloNaranja from './pages/pollo_naranja'
 export default function App() {
   return (
     <div className="app">
@@ -11,7 +11,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/recetas/arepas" element={<Arepas />} />
-          <Route path="/recetas/brownies" element={<Brownies />} />
+          <Route path="/recetas/Pollo" element={<PolloNaranja />} />
         </Routes>
       </main>
       <footer className="footer">© {new Date().getFullYear()} Mi libro de
