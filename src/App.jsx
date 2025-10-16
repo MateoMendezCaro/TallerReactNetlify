627a31030ce9140e07ec7921b10c8916e571a3aa
alejopg18 <alejopul18@gmail.com>
2025-10-15 21:06:19 -0500

    correccion despliegue receta pollo a la naranja

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import NavBar from './components/NavBar'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import PolloNaranja from './pages/pollo_naranja'
-import Arroz_con_leche from './pages/Arroz_con_leche'
+// import Arroz_con_leche from './pages/Arroz_con_leche'
 export default function App() {
   return (
     <div className="app">
@@ -10,8 +10,8 @@ export default function App() {
       <main className="container">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/recetas/arrozconleche" element={<Arroz_con_leche />} />
-          <Route path="/recetas/Pollo" element={<PolloNaranja />} />
+          {/* <Route path="/recetas/arrozconleche" element={<Arroz_con_leche />} /> */}
+          <Route path="/recetas/polloNaranja" element={<PolloNaranja />} />
         </Routes>
       </main>
       <footer className="footer">© {new Date().getFullYear()} Mi libro de
